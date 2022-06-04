34ae974482201b4f8e1f8ae8c5311915a924e39c
asdimas <asepdimas19@gmail.com>
2022-06-04 18:47:41 +0700

    Upload Project Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      fontFamily: {
+        raleway: ['Raleway'],
+      },
+      colors: {
+        primary: '#D5C7AD',
+        secondary: '#3D3B36',
+        textColor: '#5B5B5B',
+        dark: '#0f172a',
+      },
+    },
+  },
+  plugins: [],
+}
